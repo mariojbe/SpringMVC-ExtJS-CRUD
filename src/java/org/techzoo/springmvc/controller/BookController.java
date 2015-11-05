d33689f8890cca7149eb12e7fb76bac9790a08af
mariojbe <mariojbe@gmail.com>
2015-11-05 13:48:37 -0300

    Alterando XML

--- src/java/org/techzoo/springmvc/controller/BookController.java
+++ src/java/org/techzoo/springmvc/controller/BookController.java
@@ -20,53 +20,51 @@ import org.techzoo.springmvc.vo.Book;
 @RequestMapping("api")
 public class BookController {
 
-	BookDao bookBao;
-	
-	@Autowired
-	public BookController(BookDao bookBao) {
-		this.bookBao = bookBao;
-	}
-	
-	@RequestMapping(value = "/index")
-	public String index()
-	{
-		return "index";
-	}
-	
-	@RequestMapping (value = "book/save", method = RequestMethod.POST)
-	@ResponseBody
-	public boolean saveBook(@RequestBody Book book)
-	{
-		return bookBao.addBook(book);
-	}
-	
-	@ResponseBody
-	@RequestMapping (value = "book/loadBooks")
-	public Map<String, List<Book>> loadAllBooks()
-	{
-		Map<String, List<Book>> books = new HashMap<String, List<Book>>();
-		books.put("books", bookBao.listBooks());
-		return books;
-	}
-	
-	@RequestMapping (value = "book/delete", method = RequestMethod.POST)
-	@ResponseBody
-	public boolean deleteBooks(@RequestBody Book book)
-	{
-		return bookBao.removeBook(book);
-	}
-	
-	@RequestMapping (value = "book/updateBook", method = RequestMethod.POST)
-	@ResponseBody
-	public boolean updateBooks(@RequestBody Book book)
-	{
-		bookBao.updateBook(book);
-		return true;
-	}
-	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public List<ObjectError> handleMethodArgumentNotValidException(
-			MethodArgumentNotValidException error ) {
-	   return error.getBindingResult().getAllErrors();
-	}
+    BookDao bookBao;
+
+    public BookController() {
+    }
+
+    @Autowired
+    public BookController(BookDao bookBao) {
+        this.bookBao = bookBao;
+    }
+
+    @RequestMapping(value = "/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "book/save", method = RequestMethod.POST)
+    @ResponseBody
+    public boolean saveBook(@RequestBody Book book) {
+        return bookBao.addBook(book);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "book/loadBooks")
+    public Map<String, List<Book>> loadAllBooks() {
+        Map<String, List<Book>> books = new HashMap<String, List<Book>>();
+        books.put("books", bookBao.listBooks());
+        return books;
+    }
+
+    @RequestMapping(value = "book/delete", method = RequestMethod.POST)
+    @ResponseBody
+    public boolean deleteBooks(@RequestBody Book book) {
+        return bookBao.removeBook(book);
+    }
+
+    @RequestMapping(value = "book/updateBook", method = RequestMethod.POST)
+    @ResponseBody
+    public boolean updateBooks(@RequestBody Book book) {
+        bookBao.updateBook(book);
+        return true;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<ObjectError> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException error) {
+        return error.getBindingResult().getAllErrors();
+    }
 }
---
+++ web/jsp/index.jsp
@@ -0,0 +1,26 @@
+<%-- 
+    Document   : index.jsp
+    Created on : 05/11/2015, 13:46:43
+    Author     : user
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>ExtJS CRUD App</title>
+        <link rel="stylesheet" type="text/css" 
+              href="http://cdn.sencha.com/ext/gpl/4.2.0/resources/css/ext-all.css">
+        <script type="text/javascript" 
+        src="http://cdn.sencha.com/ext/gpl/4.2.0/ext-all.js"></script>
+        <style>
+            .book-add { background-image:url('resources/images/book_add.png');}
+            div#output {margin:100px;}
+        </style>
+        <script type="text/javascript" src="resources/script/books.js"></script>
+    </head>
+    <body>
+        <div id="output"></div>
+    </body>
+</html>
